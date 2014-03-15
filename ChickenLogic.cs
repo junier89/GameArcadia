ea5b60c341f1fe8c167674e7b6369ccacca589c6
junier89 <jjstocker1@gmail.com>
2014-03-15 14:29:06 -0500

    Improved main screen, updated Chicken screen
    The main screen can call the chicken screen so that the game can be
    played.

---
+++ ChickenLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameArcadia
+{
+    public class ChickenLogic
+    {
+        private int[,] SetOfCurrentDice { get; set; }
+        private int NumberOfRolls { get; set; }
+        private int Score { get; set; }
+
+        public ChickenLogic()
+        {
+            NumberOfRolls = 0;
+            Score = 0;
+            SetOfCurrentDice =
+        }
+        }
+
+        public void ChickenMain()
+        {
+            
+        }
+
+        private int RollAllDice()
+        {
+            for (var i = 0; i < 7; i++)
+            {
+                setOfCurrentDice[i,0] = RollOneDie();
+            }
+        }
+
+        private int RollOneDie()
+        {
+            var randomNumberGenerator = new Random();
+            return randomNumberGenerator.Next(1, 7);
+        }
+
+    }
+}
