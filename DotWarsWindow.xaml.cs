2f7ae4fe559e01e2682aa762cb254df29408a0c1
junier89 <jjstocker1@gmail.com>
2014-05-21 12:59:33 -0500

    Created DotWars Window and Game

---
+++ DotWarsWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameArcadia
+{
+	/// <summary>
+	/// Interaction logic for DotWarsWindow.xaml
+	/// </summary>
+	public partial class DotWarsWindow : Window
+	{
+		private DotWarsGame dotWarsGame;
+		public DotWarsWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void NewGameClick(object sender, RoutedEventArgs e)
+		{
+			dotWarsGame = new DotWarsGame();
+		}
+	}
+}
